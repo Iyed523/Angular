8fba6452906b4e29eb39a725aa001716b02987d7
Iyed523 <iyedloghmari@hotmail.com>
2025-05-26 16:23:52 +0200

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,8 @@ export const routes: Routes = [
     { path: 'home', component: AssignmentsComponent },
     { path: 'add', component: AddAssignmentComponent },
     {path: 'assignments/:id', component: AssignmentDetailComponent},
-    {path: 'assignment/:id/edit', component: EditAssignmentComponent},
-    {path: 'assignment/:id/edit', component: EditAssignmentComponent, canActivate: [authGuard]}
+    {path: 'assignment/:id/edit', component: EditAssignmentComponent, canActivate: [authGuard]},
+    { path: 'login', component: LoginComponent }
+
 
 ];
