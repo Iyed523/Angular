1a6fdcdde9cb65bd0bd4663b56df2e5ac068bb24
Iyed523 <iyedloghmari@hotmail.com>
2025-05-31 18:05:39 +0200

    commit

--- src/app/prerender.ts
+++
@@ -1,5 +0,0 @@
-export async function getPrerenderParams() {
-  const response = await fetch('http://localhost:8010/api/assignments'); 
-  const assignments = await response.json();
-  return assignments.map((a: any) => ({ id: a.id.toString() }));
-}
